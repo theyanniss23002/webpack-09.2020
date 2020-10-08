9e78543bf1d7d08070a54ec5ea45f63cd461055f
theyanniss23002 <mr.artur23002@gmail.com>
2020-10-08 15:10:18 +0300

    Configuration upgrades

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,80 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
-        filename: 'bundle.[chunkhash].js',
-        path: path.resolve(__dirname, 'public')
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    resolve: {
+        alias: {
+            'images': path.resolve(__dirname, 'src/images'),
+            'fonts': path.resolve(__dirname, 'src/fonts'),
+        },
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader']
-        },
-        {
-            test: /\.sass$/,
-            use: [
-                'style-loader',
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: { sourceMap: true }
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'images',
+                    name: '[name].[ext]'
                 },
-                {
-                    loader: 'postcss-loader',
-                    options: { sourceMap: true, config: { path: './postcss.config.js' } }
-                },
-                {
-                    loader: 'sass-loader',
-                    options: { sourceMap: true }
+            },
+            {
+                test: /\.(ttf|otf|woff)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'fonts',
+                    name: '[name].[ext]'
                 }
-            ]
-        }]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: './postcss.config.js' } }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }
+        ]
     },
     devServer: {
-        port: 3000
+        port: 4200
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -44,7 +84,15 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'app.css'
+            filename: '[name].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'public')
+                }
+            ]
         })
-    ]
+    ],
 }
